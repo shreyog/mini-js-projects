688c772ed2a2ea7e0ad3c96c35ec9852359b8729
shreyog <shreyog23@gmail.com>
2021-01-04 07:29:25 +0530

    mini project guess my number

---
+++ 01-Guess-My-Number/script.js
@@ -0,0 +1,61 @@
+'use strict';
+
+// console.log(document.querySelector('.message').textContent);
+
+// document.querySelector('.message').textContent = '🎉 Correct Number!';
+
+// console.log(document.querySelector('.message').textContent);
+
+// document.querySelector('.number').textContent = 13;
+// document.querySelector('.score').textContent = 10;
+
+// document.querySelector('.guess').value = 23;
+// console.log(document.querySelector('.guess').value);
+
+let secretNumber = Math.trunc(Math.random() * 20) + 1;
+let score = 20;
+let highScore = 0;
+
+const displayMessage = (cssClass, message) => {
+  document.querySelector(cssClass).textContent = message;
+};
+
+document.querySelector('.check').addEventListener('click', () => {
+  const guess = Number(document.querySelector('.guess').value);
+  if (!guess) {
+    displayMessage('.message', '⛔ No Number!');
+  } else if (guess === secretNumber) {
+    displayMessage('.number', secretNumber);
+    displayMessage('.message', '🎉 Correct Number!');
+    document.querySelector('body').style.backgroundColor = '#60b347';
+    document.querySelector('.number').style.width = '30rem';
+
+    if (score > highScore) {
+      highScore = score;
+      document.querySelector('.highscore').textContent = highScore;
+    }
+  } else if (guess !== secretNumber) {
+    if (score > 1) {
+      displayMessage(
+        '.message',
+        guess > secretNumber ? '📈 Too high!' : '📉 Too low!'
+      );
+      score--;
+      displayMessage('.score', score);
+    } else {
+      displayMessage('.score', 0);
+      displayMessage('.message', '💥 You lost the game!');
+    }
+  }
+});
+
+document.querySelector('.again').addEventListener('click', () => {
+  score = 20;
+  secretNumber = Math.trunc(Math.random() * 20) + 1;
+  displayMessage('.message', 'Start guessing...');
+  document.querySelector('body').style.backgroundColor = '#222';
+  document.querySelector('.number').style.width = '15rem';
+  displayMessage('.score', 20);
+  document.querySelector('.guess').value = '';
+  displayMessage('.number', '');
+});
