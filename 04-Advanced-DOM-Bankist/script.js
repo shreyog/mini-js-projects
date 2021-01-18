2ee75888bf9ca21564a03627c0a8a58efede18e3
shreyog <shreyog23@gmail.com>
2021-01-18 17:59:26 +0530

    Dom basics

--- 04-Advanced-DOM-Bankist/script.js
+++ 04-Advanced-DOM-Bankist/script.js
@@ -29,3 +29,38 @@ document.addEventListener('keydown', function (e) {
     closeModal();
   }
 });
+
+
+/////////////
+/////////////
+
+const header = document.querySelector('.header');
+
+const allSections = document.querySelectorAll('.section');
+
+console.log(allSections);
+
+document.getElementById('section--1');
+
+const allButtons = document.getElementsByTagName('button');
+
+console.log(allButtons);
+
+console.log(document.getElementsByClassName('btn'));
+
+// Creating and inserting elements
+const message = document.createElement('div');
+message.classList.add('cookie-message');
+message.innerHTML = 'text <button class="btn btn--close-cookie">Got it!</button>';
+
+// header.prepend(message);
+header.append(message);
+// header.append(message.cloneNode(true));
+
+// header.before(message);
+// header.after(message);
+
+// Delete elements
+document.querySelector('.btn--close-cookie').addEventListener('click', () => {
+  message.remove();
+})
